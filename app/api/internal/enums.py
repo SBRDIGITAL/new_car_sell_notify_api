1ff8d57026889cd680fdf9877a6fe54b61949498
Vasiliy Sibirenkov <sbrdigital@yandex.ru>
2025-11-07 17:50:06 +0300

    update structure

---
+++ app/api/internal/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class EnamExample(Enum):
+    EN_1 = 'ENUM_1'
+    EN_2 = 'ENUM_2'
+    EN_3 = 'ENUM_3'
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from new-car-sell-notify-api!")
+from typing import Union
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # До запуска приложения
+    # print('До запуска')
+    yield  # Приложение работает
+    # Выключение приложения
+    # print('После запуска')
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}

de31a9a79dd7e92d5ad7e1b7f46b63652eb4dc8a
Vasiliy Sibirenkov <sbrdigital@yandex.ru>
2025-11-07 18:24:51 +0300

    Добавлен lifecheck и реализован класс приложения

---
+++ app/api/v1/routes/lifecheck.py
@@ -0,0 +1,33 @@
+"""Модуль для проверки работоспособности `API`.
+
+Содержит эндпоинт для проверки доступности и работоспособности сервиса.
+"""
+
+from fastapi import APIRouter
+
+
+
+router = APIRouter(prefix='/lifecheck')
+
+
+
+@router.get("/")
+async def check():
+    """
+    ## Проверяет работоспособность `API`.
+
+    ### Простой эндпоинт для проверки доступности сервиса и подтверждения \
+        его корректной работы.
+
+    ### Returns:
+        dict: Словарь с ключом `'lifecheck'` и значением `True`, указывающим
+            на то, что сервис работает корректно.
+
+    ### Example:
+        ```python
+            response = await read_root()
+            print(response)
+            {"lifecheck": True}
+        ```
+    """
+    return {"lifecheck": True}
--- main.py
+++ main.py
@@ -1,26 +1,85 @@
-from typing import Union
+"""Главный модуль приложения `FastAPI` для `API` уведомлений о продаже новых автомобилей.
+
+Этот модуль инициализирует и настраивает приложение `FastAPI`, включая регистрацию
+роутеров и управление жизненным циклом приложения.
+"""
+
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+
+from app.api.v1.routes.lifecheck import router as lifecheck_router
 
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # До запуска приложения
-    # print('До запуска')
-    yield  # Приложение работает
-    # Выключение приложения
-    # print('После запуска')
+class FastAPIapp:
+    """
+    ## Класс для инициализации и настройки `FastAPI` приложения.
 
-app = FastAPI(lifespan=lifespan)
+    Управляет созданием экземпляра `FastAPI`, регистрацией роутеров и 
+    жизненным циклом приложения.
+
+    Attributes:
+        app (FastAPI): Экземпляр приложения `FastAPI`.
+        app_routers (dict[str, list[APIRouter]]): Словарь роутеров, где ключ - префикс,
+            а значение - список роутеров для этого префикса.
+    """
+
+    def __init__(self):
+        """
+        ## Инициализирует экземпляр класса.
+        """
+        self.app = FastAPI(lifespan=self.lifespan)
+        self.app_routers: dict[str, list[APIRouter]] = {
+            '/v1': [
+                lifecheck_router,
+                # more routers ...
+            ]
+        }
+        self.__post_init()
+
+    def __post_init(self):
+        """ ## Выполняет пост-инициализацию после создания экземпляра класса. """
+        self._inlude_routers()
+
+    @asynccontextmanager
+    async def lifespan(self, app: FastAPI):
+        """
+        ## Управляет жизненным циклом приложения `FastAPI`.
+
+        Контекстный менеджер для выполнения действий при запуске и остановке приложения.
+
+        Args:
+            app (FastAPI): Экземпляр приложения `FastAPI`.
+
+        Yields:
+            None: Контроль передается приложению во время его работы.
+        """
+        # До запуска приложения
+        # print('До запуска')
+        yield  # Приложение работает
+        # Выключение приложения
+        # print('После запуска')
+
+    def _inlude_routers(self):
+        """
+        ## Регистрирует все роутеры в приложении `FastAPI`.
+
+        Проходит по словарю `app_routers` и включает каждый роутер
+            с соответствующим префиксом.
+
+        Raises:
+            ValueError: Если `self.app` не является экземпляром `FastAPI`.
+        """
+        if not isinstance(self.app, FastAPI):
+            raise ValueError('Не передан объект FastAPI приложения')
+        for i in self.app_routers.items():
+            [self.app.include_router(r, prefix=i[0]) for r in i[-1]]
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+fastapi_app = FastAPIapp()
+app: FastAPI = fastapi_app.app
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# Запуск при разработке
+# uvicorn main:app --reload
